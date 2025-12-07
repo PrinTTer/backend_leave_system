42a521abc312757144edcdcde5a1a32b94a2ad48
waranyapr <waranyaprawanno@gmail.com>
2025-12-08 00:07:58 +0700

    fix: แก้ไขการสร้างใบลาและเพิ่ม find credit leave ที่เหลืออยู่ เปลี่ยนไปใช้ enableCord({}) เพื่อรองรับการเชื่อมต่อจากทุก server

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3000', // frontend ของคุณ
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // เผื่ออนาคตใช้ cookie / header auth
-  });
+  app.enableCors({});
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -17,6 +13,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
   await app.listen(process.env.PORT ?? 3001);
 }
